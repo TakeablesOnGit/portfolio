f56e3204d9ec92a28e432d26c1a76c364534d792
TakeablesOnGit <TakeablesBusiness@gmail.com>
2025-09-05 05:43:34 -0500

    Change Blog Image Size

--- src/components/sections/blogs.tsx
+++ src/components/sections/blogs.tsx
@@ -26,9 +26,7 @@ export function Blogs({ data }: { data: Record<string, IBlogData> }) {
               <img
                 src={value.IMAGE.src}
                 alt={key}
-                width={500}
-                height={500}
-                className="w-full sm:w-48 h-32 object-cover rounded-lg shadow-md"
+                className="w-full sm:w-50 h-50 object-cover rounded-lg shadow-md"
               />
               
               <p className="text-primary/90 text-lg">
